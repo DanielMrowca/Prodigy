7b0fde3a6154fdd158c39a81fc0c0862bb3b67c2
Daniel Mrowca <daniel.mrowcaa@gmail.com>
2020-03-01 19:32:11 +0100

    Added samples using HoneyComb.WebApi

---
+++ samples/WebApi/Program.cs
@@ -0,0 +1,30 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using HoneyComb;
+using HoneyComb.WebApi;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+
+namespace WebApi
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        => await CreateHostBuilder(args).Build().RunAsync();
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.ConfigureServices(services => services
+                        .AddHoneyComb()
+                        .AddWebApi())
+                    .Configure(app => app
+                        .UseEndpoints(endpoints => endpoints
+                            .Get("", ctx => ctx.Response.WriteAsync("WebApi app using HoneyComb :)"))
+                            .Get<Test>("test/{customText?}",
+                            async (query, ctx) => await ctx.Response.WriteAsync(JsonConvert.SerializeObject(query))))
+                        );
+                });
+    }
+}
---
+++ samples/WebApi/Test.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi
+{
+    public class Test
+    {
+        public DateTime DateTime => DateTime.Now;
+        public string CustomText { get; set; }
+    }
+}
