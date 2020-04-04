de4698838f74a9258986511395489533d03dc12f
Daniel Mrowca <daniel.mrowcaa@gmail.com>
2020-04-04 21:02:05 +0200

    Added custom figlet font

---
+++ src/HoneyComb.Figlet/FiggleFonts.cs
@@ -0,0 +1,13 @@
+﻿using Figgle;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace HoneyComb.Figlet
+{
+    public static class FiggleFonts
+    {
+        public static FiggleFont ANSI_Shadow => FiggleFontParser.Parse(new MemoryStream(Resource1.ANSI_Shadow));
+    }
+}
