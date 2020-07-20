14f6472d53b14823355896d17c3d550bd37aca25
daniel.mrowca <daniel.mrowca@skkglobal.com>
2020-07-20 12:46:48 +0200

    Added forever retry with polly when cannot get settings from vault

--- src/HoneyComb.Vault/Extensions.cs
+++ src/HoneyComb.Vault/Extensions.cs
@@ -4,8 +4,11 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Configuration.Memory;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using Newtonsoft.Json.Linq;
+using Polly;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using VaultSharp;
 using VaultSharp.V1.AuthMethods;
@@ -18,7 +21,7 @@ namespace HoneyComb.Vault
     {
         private const string SectionName = "vault";
         public static IWebHostBuilder UseVault(this IWebHostBuilder builder, string keyValuePath = null,
-            string sectionName = SectionName)
+            string sectionName = SectionName, bool retryOnError = true, Action<IWebHostBuilder, IServiceProvider> configure = null)
             => builder.ConfigureServices(services =>
             {
                 if (string.IsNullOrWhiteSpace(sectionName))
@@ -30,6 +33,7 @@ namespace HoneyComb.Vault
 
                 var options = config.GetSettings<VaultOptions>(sectionName);
                 services.AddSingleton(options);
+                configure?.Invoke(builder, sp);
             })
             .ConfigureAppConfiguration((ctx, cfg) =>
             {
@@ -38,22 +42,45 @@ namespace HoneyComb.Vault
                 if (!options.Enabled)
                     return;
 
-                cfg.AddVaultAsync(options, keyValuePath).GetAwaiter().GetResult();
+
+                cfg.AddVaultAsync(options, keyValuePath, retryOnError).GetAwaiter().GetResult();
                 cfg.AddJsonFile("appsettings.json", false);
                 cfg.AddJsonFile($"appsettings.{env}.json", true);
+
+                //cfg.AddVaultAsync(options, keyValuePath, retryOnError).GetAwaiter().GetResult();
+                //cfg.AddJsonFile("appsettings.json", false);
+                //cfg.AddJsonFile($"appsettings.{env}.json", true);
             });
 
 
         private static async Task AddVaultAsync(this IConfigurationBuilder builder, VaultOptions options,
-            string keyValuePath)
+            string keyValuePath, bool retryOnError)
         {
+            var logger = Logging.Extensions.BuildLoggerConfiguration().CreateLogger();
             var kvPath = string.IsNullOrWhiteSpace(keyValuePath) ? options.Kv?.Path : keyValuePath;
             var (client, _) = GetClientAndSettings(options);
             if (!string.IsNullOrWhiteSpace(kvPath) && options.Kv.Enabled)
             {
-                Console.WriteLine($"Loading settings from Vault: '{options.Url}', KV path: '{kvPath}'.");
+                logger.Information($"Loading settings from Vault: '{options.Url}', KV path: '{kvPath}'.");
                 var keyValueSecrets = new KeyValueSecret(client, options);
-                var secret = await keyValueSecrets.GetAsync(kvPath);
+                IDictionary<string, object> secret = new Dictionary<string, object>();
+
+                if (retryOnError)
+                {
+                    var policyResult = await Policy
+                        .Handle<Exception>()
+                        .WaitAndRetryForeverAsync((r, e, ctx) => TimeSpan.FromSeconds(3 * r), OnRetryException)
+                        .ExecuteAndCaptureAsync(async () => await keyValueSecrets.GetAsync(kvPath));
+
+                    secret = policyResult.Result;
+                    Task OnRetryException(Exception ex, int r, TimeSpan ts, Context ctx)
+                    {
+                        logger.Error($"Retry [ {r} / ∞ ]. Error while loading settings from Vault: '{options.Url}', KV path: '{kvPath}'. {ex.Message}");
+                        return Task.CompletedTask;
+                    }
+                }
+
+                secret = await keyValueSecrets.GetAsync(kvPath);
                 var parser = new JsonParser();
                 var data = parser.Parse(JObject.FromObject(secret));
                 var source = new MemoryConfigurationSource { InitialData = data };
