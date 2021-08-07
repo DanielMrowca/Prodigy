5feb0511f90cb475f75140a3a11b0c2634ed7f8f
Daniel Mrowca <daniel.mrowcaa@gmail.com>
2021-08-07 18:40:26 +0200

    Added DefaultModelBinder dedicated for empty or unknown http content case

--- src/HoneyComb.WebApi/ModelBinding/DefaultModelBinderProvider.cs
+++ src/HoneyComb.WebApi/ModelBinding/DefaultModelBinderProvider.cs
@@ -10,8 +10,12 @@ namespace HoneyComb.WebApi.ModelBinding
         public IModelBinder GetModelBinder(HttpContext httpContext)
         {
             var contentType = httpContext.Request.ContentType;
+            var contentLength = httpContext.Request.ContentLength;
+            var isEmptyContent = contentLength is null || contentLength is 0;
 
-            if (contentType is null || contentType.Contains(MediaTypeNames.Application.Json, StringComparison.OrdinalIgnoreCase))
+            if (contentType is null || isEmptyContent)
+                return new DefaultModelBinder();
+            if ( contentType.Contains(MediaTypeNames.Application.Json, StringComparison.OrdinalIgnoreCase))
                 return new JsonModelBinder();
             if(contentType.Contains("multipart/form-data", StringComparison.OrdinalIgnoreCase))
                 return new FormFileModelBinder();
